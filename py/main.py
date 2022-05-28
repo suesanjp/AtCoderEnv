73bcf7755bd6807afc2bcbbfc29ee478ddacab92
Tomoaki Suzuki <sue@Tomoakis-MacBook-Pro.local>
2022-05-29 00:32:57 +0900

    edited template's input styles

--- py/main.py
+++ py/main.py
@@ -1,8 +1,19 @@
 # 整数の入力
+from posixpath import split
+
 a = int(input())
+
 # スペース区切りの整数の入力
 a, b = map(int, input().split())
-# 文字列の入力
-s = input()
 
-print('')
+
+# スペース区切りの整数の入力をリストにする
+a = list(map(int, input().split()))
+
+# スペース区切りの文字列の入力をリストにする
+s = input().split()
+
+# n個の要素をリストに格納
+n = int(input())
+s = [input() for _ in range(n)]
+
